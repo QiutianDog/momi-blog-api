c295c72fb538fcab2b772f34d13afa4cc25ee0f0
QiutianDog <1019652866@qq.com>
2022-01-13 14:30:23 +0800

    init commit

---
+++ src/main/java/fun/qiutiandog/moniblogapi/MoniBlogApiApplication.java
@@ -0,0 +1,13 @@
+package fun.qiutiandog.moniblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoniBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoniBlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/qiutiandog/moniblogapi/MoniBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.qiutiandog.moniblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoniBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
